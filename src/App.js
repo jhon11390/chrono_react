24cebfe0b23f5eeb0361e4b3e7509617d7ceef49
jhon11390 <meljhon2@gmail.com>
2020-06-28 15:03:12 -0500

    Agrega paquete de icons-react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {Container, Row, Col} from "react-bootstrap";
+import { FaTrashAlt, FaEdit} from 'react-icons/fa';
 import './App.css';
 
 function App() {
   return (
-    <h1>chrono</h1>
+    <Container fluid className="mt-4">
+      <Row className="justify-content-md-center">
+        <Col md="auto">
+          <h1>Timers</h1>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
